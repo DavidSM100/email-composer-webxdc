a80c4447a350a447b85100695c17a709c0878bc8
DavidSM100 <d.s.m.011010@gmail.com>
2025-01-19 13:30:00 -0500

    reduce bundle size

--- src/js/editor.js
+++ src/js/editor.js
@@ -1,18 +1,47 @@
 import Quill from "quill";
 import QuillTableBetter from "quill-table-better";
 import BlotFormatter from "quill-blot-formatter-mobile";
-import hljs from "highlight.js";
+import hljs from "highlight.js/lib/core";
+import bashLang from "highlight.js/lib/languages/bash";
+import cppLang from "highlight.js/lib/languages/cpp";
+import csLang from "highlight.js/lib/languages/csharp";
+import cssLang from "highlight.js/lib/languages/css";
+import diffLang from "highlight.js/lib/languages/diff";
+import htmlLang from "highlight.js/lib/languages/vbscript-html";
+import xmlLang from "highlight.js/lib/languages/xml";
+import javaLang from "highlight.js/lib/languages/java";
+import javascriptLang from "highlight.js/lib/languages/javascript";
+import markdownLang from "highlight.js/lib/languages/markdown";
+import phpLang from "highlight.js/lib/languages/php";
+import pythonLang from "highlight.js/lib/languages/python";
+import rubyLang from "highlight.js/lib/languages/ruby";
+import sqlLang from "highlight.js/lib/languages/sql";
 import "quill/dist/quill.snow.css";
 import "quill-table-better/dist/quill-table-better.css";
-import "highlight.js/styles/atom-one-dark.css";
+import "highlight.js/styles/github-dark.css";
 import undoImg from "../imgs/undo.svg?raw";
 import redoImg from "../imgs/redo.svg?raw";
 import clearImg from "../imgs/clear.svg?raw";
 import quillStyles from "quill/dist/quill.snow.css?inline";
 import tableSyles from "quill-table-better/dist/quill-table-better.css?inline";
-import highlightStyles from "highlight.js/styles/atom-one-dark.css?inline";
+import highlightStyles from "highlight.js/styles/github-dark.css?inline";
 
 export { startEditor, quillStyles, tableSyles, highlightStyles };
+
+hljs.registerLanguage("bash", bashLang);
+hljs.registerLanguage("cpp", cppLang);
+hljs.registerLanguage("cs", csLang);
+hljs.registerLanguage("css", cssLang);
+hljs.registerLanguage("diff", diffLang);
+hljs.registerAliases("html", htmlLang);
+hljs.registerLanguage("xml", xmlLang);
+hljs.registerLanguage("java", javaLang);
+hljs.registerLanguage("javascript", javascriptLang);
+hljs.registerLanguage("markdown", markdownLang);
+hljs.registerLanguage("php", phpLang);
+hljs.registerLanguage("python", pythonLang);
+hljs.registerLanguage("ruby", rubyLang);
+hljs.registerLanguage("sql", sqlLang);
 
 const toolbarOptions = [
   ["undo", "redo"],
@@ -83,6 +112,6 @@ function startEditor() {
   }
 
   function clear() {
-    editor.setContents([{insert: "\n"}])
+    editor.setContents([{ insert: "\n" }]);
   }
 }
