fbc9eeb468bdbd4afeb660d9bade7d64cc916115
DavidSM100 <d.s.m.011010@gmail.com>
2025-01-19 15:38:13 -0500

    clearly show when subject and html are not supported and disable them

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import { readableSize } from "./util.js";
 import localforage from "localforage";
 import {
   startEditor,
+  baseConfig,
   quillStyles,
   tableSyles,
   highlightStyles,
@@ -13,7 +14,16 @@ const fileBtn = document.getElementById("fileBtn");
 const fileText = document.getElementById("fileText");
 let currentFile = null;
 
-startEditor();
+if (window.webxdc.arcanechat) {
+  startEditor("#editor", baseConfig);
+} else {
+  subjectInput.placeholder += " (Not supported)";
+  subjectInput.disabled = true;
+  baseConfig.placeholder += " (Not supported)";
+  const editor = startEditor("#editor", baseConfig);
+  editor.disable();
+}
+
 const htmlDiv = document.getElementById("editor").firstChild;
 
 loadSavedData();
